6bbf512678e1561d3923358c713b385f4fcdb653
Gabriel Duarte <gabriel160499@gmail.com>
2017-11-23 09:25:58 -0200

    Correcao de bug na hora da compra e optimzacao

--- Mercado.c
+++ Mercado.c
@@ -20,11 +20,6 @@
 #include <string.h>
 #include <stdbool.h>
 #define QuantMax 200
-#include <setjmp.h>
-#define TRY do{ jmp_buf ex_buf__; if( !setjmp(ex_buf__) ){
-#define CATCH } else {
-#define ETRY } }while(0)
-#define THROW longjmp(ex_buf__, 1)
 
 int produtosCadastrados = 0;
 
